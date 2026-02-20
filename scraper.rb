9c424b4ddbfae8e6a8ad5d8f8f29c12e1a0d0b9a
kazuhiro-cab <kaduui@gmail.com>
2026-02-20 12:44:37 +0900

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,35 +1,43 @@
 require 'open-uri'
 require 'nokogiri'
+require 'sqlite3'
+require 'time'
 
 URL = "https://www.dlsite.com/maniax/new/=/work_type_category/illust"
+DB_FILE = "dlsite.db"
+
+db = SQLite3::Database.new(DB_FILE)
+
+db.execute <<-SQL
+CREATE TABLE IF NOT EXISTS works (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  fetched_at TEXT,
+  product_id TEXT UNIQUE,
+  title TEXT,
+  url TEXT
+);
+SQL
 
 html = URI.open(URL).read
 doc = Nokogiri::HTML(html)
 
 items = doc.css(".n_worklist_item")
 
-rss = <<~XML
-<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-<channel>
-<title>DLsite Illust New</title>
-<link>#{URL}</link>
-<description>DLsite CG New Works</description>
-XML
-
-items.first(20).each do |item|
+items.each do |item|
   title = item.css(".work_name").text.strip
   link = item.css(".work_name a").attr("href")&.value
   next unless link
 
-  rss += <<~ITEM
-  <item>
-    <title>#{title}</title>
-    <link>#{link}</link>
-  </item>
-  ITEM
+  product_id = link.match(/RJ\d+/)&.to_s
+  next unless product_id
+
+  begin
+    db.execute(
+      "INSERT INTO works (fetched_at, product_id, title, url) VALUES (?, ?, ?, ?)",
+      [Time.now.iso8601, product_id, title, link]
+    )
+  rescue SQLite3::ConstraintException
+  end
 end
 
-rss += "</channel></rss>"
-
-File.write("rss.xml", rss)
+puts "DB updated"
